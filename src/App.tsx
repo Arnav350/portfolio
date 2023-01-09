5e3a095c38617895bdabe3eb32b0c8450a489b46
Arnav Patel <patelarnavm+github@gmail.com>
2023-01-08 22:35:32 -0500

    Started intro, created title, subtitle, and burger

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
   return (
     <main>
-      <nav>nav</nav>
-      <section>sec</section>
+      <section className="intro">
+        <div className="intro__circle">
+          <button className="nav__burger">
+            <div className="nav__line"></div>
+          </button>
+        </div>
+        <div className="intro__text">
+          <h1 className="intro__title">
+            Hello, <span className="secondary">I'm Arnav</span>
+          </h1>
+          <h3 className="intro__sub">
+            I am a <span className="secondary">frontend</span> software
+            developer based in the United States, specializing in the creation
+            of <span className="secondary">remarkable</span> online journies!
+          </h3>
+          <h3 className="intro__sub">
+            Interested in working together? Let's have a{" "}
+            <span className="secondary">talk.</span>
+          </h3>
+        </div>
+      </section>
       <section>sec</section>
       <footer>footer</footer>
     </main>
